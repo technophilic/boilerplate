2a518eeb1672cedb75939cc2d2f1ed22e8a32986
technophilic <svineth.face@gmail.com>
2019-09-04 07:41:27 +0530

    boilerplate initialized

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+/**
+ * @name handleFail
+ * @param err - error thrown by any function
+ * @description Helper function to handle errors
+ */
+let handleFail = function(err){
+    console.log("Error : ", err);
+};
+
+// Queries the container in which the remote feeds belong
+let remoteContainer= document.getElementById("remote-container");
+
+/**
+ * @name addVideoStream
+ * @param streamId
+ * @description Helper function to add the video stream to "remote-container"
+ */
+function addVideoStream(streamId){
+    let streamDiv=document.createElement("div"); // Create a new div for every stream
+    streamDiv.id=streamId;                       // Assigning id to div
+    streamDiv.style.transform="rotateY(180deg)"; // Takes care of lateral inversion (mirror image)
+    remoteContainer.appendChild(streamDiv);      // Add new div to container
+}
+/**
+ * @name removeVideoStream
+ * @param evt - Remove event
+ * @description Helper function to remove the video stream from "remote-container"
+ */
+function removeVideoStream (evt) {
+    let stream = evt.stream;
+    stream.stop();
+    let remDiv=document.getElementById(stream.getId());
+    remDiv.parentNode.removeChild(remDiv);
+    console.log("Remote stream is removed " + stream.getId());
+}
+
+// ----------- Code here -----------
